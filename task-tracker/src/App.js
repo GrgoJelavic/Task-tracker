66f327b926ffe444244f167caacf20e3acf86704
grgojelavic <grgo.jelavic@aspira.hr>
2021-03-16 23:29:11 +0100

    01-header-button-tasks

---
+++ task-tracker/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header.js';
+import Tasks from './components/Tasks.js';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
