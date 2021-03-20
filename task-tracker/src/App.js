82e0153b6ffc105801bfcfb6ee33c150aa77ae00
grgojelavic <grgo.jelavic@aspira.hr>
2021-03-20 12:09:40 +0100

    02- task, deleteTask, addTask, toggleReminder

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,47 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header.js';
 import Tasks from './components/Tasks.js';
+import AddTask from './components/AddTask.js';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className='container'>
-      <Header />
-      <Tasks />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No tasks to show'
+      )}
     </div>
   );
 }
--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -1,31 +1,15 @@
-import { useState } from 'react';
+import Task from './Task';
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'New job appointment',
-      day: 'Feb 4th at 2:22',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at Univeristy',
-      day: 'Feb 5th at 4:44',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Groceries shopping',
-      day: 'Feb 6th at 3:33',
-      reminder: false,
-    },
-  ]);
-
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
